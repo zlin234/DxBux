c7a73df24ecb5af095fd7df5dce563607ea81a0e
zlin234 <zlin2eq.edu.au@gmail.com>
2025-06-19 18:19:21 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2872,7 +2872,8 @@ def home():
     return "I'm alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
     t = Thread(target=run)
